183a46dbc140e92f9a330111563cecc6d472af0c
Betelhemmelkamu <melakmubetty@gmail.com>
2023-03-16 07:46:46 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#includ<stdio.h>
+/*
+ * main A program that prints a line with put function
+ * return : 0(Success)
+ */
+int main(void)
+{
+	put("\n"Programming is like building a multilingual puzzle");
+return (0);
+}
