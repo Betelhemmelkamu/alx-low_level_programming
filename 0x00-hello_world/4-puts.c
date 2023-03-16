f50123c5180afe235889e49a1e54b1c1194fb87e
Betelhemmelkamu <melakmubetty@gmail.com>
2023-03-16 09:16:21 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,7 +2,8 @@
 
 
 /**
- * main - prints"programming is like building a multilingual puzzle, followed by a new line."
+ * main - prints"programming is like building a multilingual 
+ * puzzle, followed by a new line."
  * Return: Always 0.
  */
 
