3fb670bcee41ac54093fd0f71edf80302aaf8df6
Betelhemmelkamu <melakmubetty@gmail.com>
2023-03-16 20:08:09 -0700

    101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main - Entry program
+ * Return: 1 (succcess)
+ */
+int main(void)
+{
+	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\r", stdout);
+	return (1);
+}
